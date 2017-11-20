b7e4c2e8b05051ba117cebdaf42b78a7cf159261
Dr. Firas Al-Hawari <firas.alhawari@gju.edu.jo>
2017-11-20 22:58:17 +0200

    Cleanup SessionBean

--- src/java/beans/SessionBean.java
+++ src/java/beans/SessionBean.java
@@ -142,46 +142,6 @@ public class SessionBean implements Serializable {
         }
     }
 
-    public void bus_position() {
-        navigate("/bus_reservation/student_pages/bus_position.xhtml");
-    }
-
-    public void bus_roadmap() {
-        navigate("/bus_reservation/student_pages/road_map.xhtml");
-    }
-
-    public void bus_seatRes() {
-        navigate("/bus_reservation/student_pages/seat_reservation.xhtml");
-    }
-
-    public void bus_checkin() {
-        navigate("/bus_reservation/student_pages/check_in.xhtml");
-    }
-
-    public void bus_status() {
-        navigate("/bus_reservation/driver_pages/bus_status.xhtml");
-    }
-    
-        public void Contact_us() {
-        navigate("/bus_reservation/bus_reservation.xhtml");
-    }
-
-    public void bus_std_info() {
-        navigate("/bus_reservation/admin_pages/student_info.xhtml");
-    }
-
-    public void bus_info() {
-        navigate("/bus_reservation/admin_pages/bus_info.xhtml");
-    }
-
-    public void bus_driver_info() {
-        navigate("/bus_reservation/admin_pages/driver_info.xhtml");
-    }
-
-    public void bus_admin_position() {
-        navigate("/bus_reservation/admin_pages/bus_position.xhtml");
-    }
-
     public void navigate(String url) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
 
